216cab0d16a3e5e3cdf040bc057772367aa5afef
Mariano Mercado <maomercado12@gmail.com>
2023-06-11 22:12:59 -0300

    correccion de errores

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
 const BtnBuscar = document.getElementById("BtnBuscar");
 const InpTxt = document.getElementById("InpTxt");
 const cardContainer = document.querySelector(".card-container");
+const Smallmgs = document.querySelector(".mgs");
 
 const getPoliwrath = async (id) => {
-  const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
-  const data = await response.json();
-  console.log(data);
-  return data;
+  try {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+    const data = await response.json();
+    console.log(data);
+    return data;
+  } catch (error) {
+    showError(`Error al buscar :${error} `);
+  }
 };
 
 /*Lo mínimo que deberá tener la card es el nombre, su tipo principal (pueden intentar poner todos) , su altura y peso (expresada en metros y kilogramos, tendrán que dividir el alto y peso que les llegue por 10), y una de sus imágenes.* */
@@ -19,8 +24,15 @@ const pokemonData = (pokemon) => {
     weight: pokemon.weight / 10,
     // types: pokemon.types,
     experience: pokemon.base_experience,
-    types: pokemon.types.map((type) => type.name),
+    types: pokemon.types.map((type) => type.type.name),
   };
+};
+const createTypeCards = (types) => {
+  return types
+    .map((tipo) => {
+      return `<p>Tipo : ${tipo.type.name}</p>`;
+    })
+    .join("");
 };
 const templatePokemon = (pokemon) => {
   const { name, sprites, types, height, weight } = pokemon;
@@ -31,7 +43,8 @@ const templatePokemon = (pokemon) => {
             <h2>${name}</h2>
             <p>Alto: ${height} m</p>
             <p>Ancho: ${weight} kg</p>
-            <p>Tipos: ${types.join(", ")}</p>S
+               ${createTypeCards(types)}
+
         </div>
     `;
   cardContainer.innerHTML = pokemonHTML;
@@ -50,15 +63,20 @@ const isEmptyInput = () => {
 };
 
 // Funcion si no existe la city
-const isInvaliPoke = (pokeapi) => !pokeapi.id;
+const isInvaliPoke = (pokeapi) => {
+  console.log("llego");
+  return !pokeapi.id;
+};
 /* Con el número que se ponga, hacer una llamada a la pokeapi y renderizar una card con los datos del Pokémon encontrado.*/
 
-const searchCity = async (e) => {
-  debugger;
-
+const showError = (message) => {
+  Smallmgs.textContent = message;
+};
+const searchPokemon = async () => {
+  Smallmgs.textContent = "";
   //   Validacion si el input está vacio
   if (isEmptyInput()) {
-    alert("Debes ingresar una ciudad");
+    showError("Por favor, ingrese un número");
     return;
   }
 
@@ -67,15 +85,15 @@ const searchCity = async (e) => {
 
   //  Validamos que exista la ciudad
   if (isInvaliPoke(fetchedPoke)) {
-    alert("Pokemon no encontrado");
-    InpTxt.value = "";
+    showError("Pokemon no encontrado");
+
     return;
   }
   renderPokemon(InpTxt.value);
 };
 
 const init = () => {
-  BtnBuscar.addEventListener("click", searchCity);
+  BtnBuscar.addEventListener("click", searchPokemon);
 };
 
 init();
